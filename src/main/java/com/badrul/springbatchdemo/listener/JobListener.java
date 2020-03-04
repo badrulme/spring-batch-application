994ef3effb373ab509e8407c760563dad0253ca7
badrul.me <202899@Hh>
2020-03-04 09:09:03 +0600

    Job Listener Completed

--- src/main/java/com/badrul/springbatchdemo/listener/JobListener.java
+++ src/main/java/com/badrul/springbatchdemo/listener/JobListener.java
@@ -1,5 +1,6 @@
 package com.badrul.springbatchdemo.listener;
 
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 
@@ -7,11 +8,18 @@ public class JobListener extends JobExecutionListenerSupport {
 
     @Override
     public void beforeJob(JobExecution jobExecution) {
-        System.out.println("In Listener Before Run Job");
+        System.out.println("In Listener Before Run " + jobExecution.getJobInstance().getJobName() + "Job");
+        jobExecution.getExecutionContext().putString("beforeJob", "beforeValue");
+
     }
 
     @Override
     public void afterJob(JobExecution jobExecution) {
-        System.out.println("In Listener After Run Job");
+        System.out.println("In Listener After Run Job. The value of before key " + jobExecution.getExecutionContext().getString("beforeJob"));
+        if (jobExecution.getStatus().equals(BatchStatus.COMPLETED)) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Failed!");
+        }
     }
 }
